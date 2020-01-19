12a37cee1326000f1cf567006aa050c398a6ea6d
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-01-19 20:44:35 -0300

    Update main page to show dev on map with profile

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-function Main() {
+function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect (() => {
@@ -33,12 +33,54 @@ function Main() {
     return null;
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -23.6453854, longitude: -46.7463694 }}>
+        <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/11967686?s=460&v=4' }}/>
+
+        <Callout onPress={() => {
+          navigation.navigate('Profile', { github_username: 'alexandremrt'});
+        }}>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Alexandre Teixeira</Text>
+            <Text style={styles.devBio}>Ruby on rails developers</Text>
+            <Text style={styles.devTechs}>Java, ruby</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
   map: {
     flex: 1
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF'
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
   },
 });
 
