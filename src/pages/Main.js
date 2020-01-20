d4bb6e91b2a77cba3b8a329ca38f7b6d62234db3
Alexandre Teixeira <alexandre@loldesign.com.br>
2020-01-19 21:34:08 -0300

    Add search bar to main page with style

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -34,21 +35,36 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -23.6453854, longitude: -46.7463694 }}>
-        <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/11967686?s=460&v=4' }}/>
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -23.6453854, longitude: -46.7463694 }}>
+          <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/11967686?s=460&v=4' }}/>
 
-        <Callout onPress={() => {
-          navigation.navigate('Profile', { github_username: 'alexandremrt'});
-        }}>
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Alexandre Teixeira</Text>
-            <Text style={styles.devBio}>Ruby on rails developers</Text>
-            <Text style={styles.devTechs}>Java, ruby</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+          <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username: 'alexandremrt'});
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Alexandre Teixeira</Text>
+              <Text style={styles.devBio}>Ruby on rails developers</Text>
+              <Text style={styles.devTechs}>Java, ruby</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+          <TextInput
+           style={styles.searchInput}
+           placeholder="Buscar devs por tech..."
+           placeholderTextColor="#999"
+           autoCapitalize="words"
+           autoCorrect={false}
+          />
+
+          <TouchableOpacity onPresss={() => {}} style={styles.loadButton}>
+            <MaterialIcons name="my-location" size={20} color="#FFF" />
+          </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -82,6 +98,44 @@ const styles = StyleSheet.create({
   devTechs: {
     marginTop: 5,
   },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+    },
+
+    searchInput: {
+      flex: 1,
+      height: 50,
+      backgroundColor: '#FFF',
+      color: '#333',
+      borderRadius: 25,
+      paddingHorizontal: 20,
+      fontSize: 16,
+      shadowColor: '#000',
+      shadowOpacity: 0.2,
+      shadowOffset: {
+        width: 4,
+        height: 4,
+      },
+      elevation: 2,
+    },
+
+    loadButton: {
+      width: 50,
+      height: 50,
+      backgroundColor: '#8E4Dff',
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 15,
+
+    },
+
 });
 
 export default Main;
